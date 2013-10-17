6d98c475d480d0b95eb70c01bae10fe4518543fb
Chris911 <Christophe.naud.dulude@gmail.com>
2013-10-17 02:24:25 -0400

    Work on caching for services

--- lib/notifyMe/services/reddit.rb
+++ lib/notifyMe/services/reddit.rb
@@ -5,8 +5,17 @@ include Mongo
 
 mongo_client = MongoClient.new.db("NotifyMe")
 requests_coll = mongo_client.collection("requests")
+requests = requests_coll.find("service" => "reddit").to_a
 
-requests = requests_coll.find("type" => "reddit").to_a
+reddit_client = Snoo::Client.new
+
 requests.each do |request|
-  puts request['request']
+  if request['type'] === 'reddit-front-page'
+    front_page = reddit_client.get_listing
+    #puts JSON.pretty_unparse(front_page)
+
+    front_page['data']['children'].each do |submission|
+      puts submission['data']['title'] + " " + submission['data']['ups'].to_s
+    end
+  end
 end
